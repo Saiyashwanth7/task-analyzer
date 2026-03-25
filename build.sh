fec2a8f74d9c607eb8732ad7a99853a346b4ae5b
Dasari Sai Yashwanth <116937254+Saiyashwanth7@users.noreply.github.com>
2026-03-25 09:57:59 +0530

    Implement superuser creation in build script
    Add script to create a superuser if it doesn't exist.

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+# Create a superuser if it doesn't exist (using environment variables)
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="$ADMIN_USERNAME").exists():
+    User.objects.create_superuser("$ADMIN_USERNAME", "$ADMIN_EMAIL", "$ADMIN_PASSWORD")
+END
