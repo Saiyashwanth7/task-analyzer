17534f0eed27d5a1cf84e6eb5efa55a489db5c01
Dasari Sai Yashwanth <116937254+Saiyashwanth7@users.noreply.github.com>
2026-03-27 19:26:01 +0530

    Refactor superuser creation in build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,18 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# This applies migrations to your Render PostgreSQL DB
 python manage.py migrate
-# Create a superuser if it doesn't exist (using environment variables)
+
+# This creates your login using the Env Vars you set in Render
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username="$ADMIN_USERNAME").exists():
-    User.objects.create_superuser("$ADMIN_USERNAME", "$ADMIN_EMAIL", "$ADMIN_PASSWORD")
+import os
+username = os.environ.get("ADMIN_USERNAME")
+email = os.environ.get("ADMIN_EMAIL")
+password = os.environ.get("ADMIN_PASSWORD")
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
 END
